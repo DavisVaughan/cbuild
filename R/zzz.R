2e34bc46a7acddce95aa764326cf3e5c646eb00f
DavisVaughan <davis@rstudio.com>
2019-12-11 12:49:08 -0500

    Add compat `startsWith()` and `endsWith()`

---
+++ R/zzz.R
@@ -0,0 +1,24 @@
+# nocov start
+.onLoad <- function(libname, pkgname) {
+  if (getRversion() <= '3.2.0') {
+    startsWith <- function (x, prefix){
+      if (!is.character(x) || !is.character(prefix)) {
+        stop("non-character object(s)")
+      }
+      suppressWarnings(substr(x, 1L, nchar(prefix)) == prefix)
+    }
+
+    endsWith <- function (x, suffix) {
+      if (!is.character(x) || !is.character(suffix)) {
+        stop("non-character object(s)")
+      }
+      n = nchar(x)
+      suppressWarnings(substr(x, n - nchar(suffix) + 1L, n) == suffix)
+    }
+
+    assign("startsWith", startsWith, envir = asNamespace("cbuild"))
+    assign("endsWith", endsWith, envir = asNamespace("cbuild"))
+  }
+}
+
+# nocov end
